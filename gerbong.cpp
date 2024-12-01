dbe9b6159dc7c635ec25d522bde1f133916533d0
Yudhistira Maulana Samaratungga <yudhistirasamaratungga@gmail.com>
2024-12-01 22:11:10 +0700

    Update gerbong.cpp

---
+++ gerbong.cpp
@@ -0,0 +1,39 @@
+#include "gerbong.h"
+
+void createListGerbong(ListGerbong &L) {
+    first(L) = NULL;
+}
+
+adr_gerbongP createElemenGerbong(infotype X) {
+    adr_gerbongP P = new elemengerbong;
+    info(P) = X;
+    next(P) = NULL;
+    prev(P) = NULL;
+    P->penumpangList = new ListPenumpang;
+    createListPenumpang(*P->penumpangList);  
+    return P;
+}
+
+void InsertLastG(ListGerbong &L, adr_gerbongP P) {
+    if (first(L) == NULL) {
+        first(L) = P;
+    } else {
+        adr_gerbongP temp = first(L);
+        while (next(J) != NULL) {
+            J = next(J);
+        }
+        next(J) = P;
+        prev(P) = J;
+    }
+}
+
+void ShowGerbong(ListGerbong L) {
+    adr_gerbongP P = first(L);
+    while (P != NULL) {
+        cout << "Kode Gerbong: " << info(P).Kode_Gerbong << endl;
+        cout << "Kapasitas: " << info(P).Kapasitas << endl;
+        ShowAll(*P->penumpangList);  
+        P = next(P);
+    }
+}
+
