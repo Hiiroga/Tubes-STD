6d9314f73fa06a3946bd34837d18eef5dfc4b0e9
Yudhistira Maulana Samaratungga <yudhistirasamaratungga@gmail.com>
2024-12-07 11:05:43 +0700

    Update gerbong.h

--- gerbong.h
+++ gerbong.h
@@ -1,21 +1,20 @@
 #ifndef GERBONG_H_INCLUDED
 #define GERBONG_H_INCLUDED
 
-#define info(P) (P)->info
-#define next(P) (P)->next
-#define prev(P) (P)->prev
-#define first(L) L.first
-#define last(L) L.last
-#define parent(P) P->parent
-
 #include <iostream>
 using namespace std;
 
+#define info(P) (P)->info
+#define next(P) (P)->next
+#define prev(P) (P)->prev
+#define first(L) (L).first
+#define last(L) (L).last
+#define parent(P) (P)->parent
 
 struct gerbong {
     string Kode_Gerbong;
     int Kapasitas;
-    int Jumlah_Penumpang; 
+    int Jumlah_Penumpang;
 };
 
 typedef gerbong infotype;
@@ -29,15 +28,6 @@ struct elemengerbong {
 struct ListGerbong {
     adr_gerbongP first;
 };
-
-
-void createListGerbong(ListGerbong &L);
-adr_gerbongP createElemenGerbong(infotype X);
-void InsertLastG(ListGerbong &L, adr_gerbongP P);
-void ShowGerbong(ListGerbong L);
-adr_gerbongP SearchG(ListGerbong L, string kode);
-void DeleteAfterG(ListGerbong &L, string kodePrec, adr_gerbongP &P); // new
-
 
 struct penumpang {
     string ID_pnp;
@@ -61,17 +51,30 @@ struct ListPenumpang {
     adr_penumpangP last;
 };
 
+void createListGerbong(ListGerbong &L);
+adr_gerbongP createElemenGerbong(infotype X);
+void InsertLastG(ListGerbong &L, adr_gerbongP P);
+void ShowGerbong(ListGerbong L);
+adr_gerbongP SearchG(ListGerbong L, string kode);
+void DeleteParentAndChild(ListGerbong &L, ListPenumpang &L, string kodeGerbong);
+
 
 void createListPenumpang(ListPenumpang &L);
 adr_penumpangP createElemenPenumpang(infotypeP X);
 void InsertLastP(ListPenumpang &L, adr_penumpangP P);
 void ShowPenumpang(ListPenumpang L, adr_gerbongP G);
 adr_penumpangP SearchP(ListPenumpang L, string ID);
-void DeleteAfterP(ListPenumpang &L, string IDPrec, adr_penumpangP &P); //new
 
 
-void connect(ListGerbong G, adr_penumpangP &C);
-void disconnect(adr_penumpangP &C);
+void UpdateGerbong(ListGerbong &L, string kodeGerbong);
+void UpdatePenumpang(ListPenumpang &L, string IDPenumpang);
+void DeleteChildInParent(ListPenumpang &L, string IDPenumpang, adr_gerbongP G);
+int CountChildInParent(ListPenumpang L, adr_gerbongP G);
+void DeleteAfterP(ListPenumpang &L, adr_penumpangP Prec);
+void connect(ListGerbong &L, adr_penumpangP &P);
+void disconnect(adr_penumpangP &P);
+
+
 int menuutama();
 void showP(ListPenumpang L);
 
